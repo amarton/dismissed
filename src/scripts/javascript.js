f3161ba249b0629fb5b5ed9e357dd4f92f311de4
Adam Marton <adam@adammarton.com>
2017-04-18 16:42:21 -0400

    responsive fixes to graphics, added social images

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -9,8 +9,8 @@ var app = app || {
 
 		$(".icon-twitter").on("click", function(){
 
-			var tweet = "Placeholder text"; //Tweet text
-			var url = "#"; //Interactive URL
+			var tweet = "Dismissed: tenants lose, landlords win in Baltimore rent court"; //Tweet text
+			var url = "http://data.baltimoresun.com/news/dismissed/"; //Interactive URL
 
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
 			window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
@@ -19,10 +19,10 @@ var app = app || {
 
 		$(".icon-facebook").on("click", function(){
 
-			var picture = "http://data.baltimoresun.com/news/dismissed/dist/images/example.jpg"; //Picture URL
-			var title = "Title here"; //Post title
-			var description = "Placeholder text"; //Post description
-			var url = "#"; //Interactive URL
+			var picture = "http://data.baltimoresun.com/news/dismissed/dist/images/facebook-thumb-rent-court.png"; //Picture URL
+			var title = "Dismissed"; //Post title
+			var description = "Tenants lose, landlords win in Baltimore rent court"; //Post description
+			var url = "http://data.baltimoresun.com/news/dismissed/"; //Interactive URL
 
 	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
