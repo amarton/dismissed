f97cec65ad59b0f1bb63e2a02abedd8ca371caa7
Adam Marton <adam@adammarton.com>
2017-04-26 15:09:34 -0400

    header tweak

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -9,7 +9,7 @@ var app = app || {
 
 		$(".icon-twitter").on("click", function(){
 
-			var tweet = "Dismissed: Tenants lose, landlords win in Baltimore rent court. %23rentcourt"; //Tweet text
+			var tweet = "Dismissed: Tenants lose, landlords win in Baltimore's rent court. %23rentcourt"; //Tweet text
 			var url = "http://data.baltimoresun.com/news/dismissed/"; //Interactive URL
 
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
@@ -20,7 +20,7 @@ var app = app || {
 		$(".icon-facebook").on("click", function(){
 
 			var picture = "http://data.baltimoresun.com/news/dismissed/dist/images/facebook-thumb-rent-court.png"; //Picture URL
-			var title = "Dismissed: Tenants lose, landlords win in Baltimore rent court"; //Post title
+			var title = "Dismissed: Tenants lose, landlords win in Baltimore's rent court"; //Post title
 			var description = "Read the Baltimore Sun investigation"; //Post description
 			var url = "http://data.baltimoresun.com/news/dismissed/"; //Interactive URL
 
