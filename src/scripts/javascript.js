37ee762edd5d44951785681b66a146cf9e154a06
Adam Marton <adammarton@adams-mbp.usa.tribune.com>
2017-04-26 11:32:37 -0400

    copy changes

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -9,7 +9,7 @@ var app = app || {
 
 		$(".icon-twitter").on("click", function(){
 
-			var tweet = "Dismissed: tenants lose, landlords win in Baltimore rent court"; //Tweet text
+			var tweet = "Dismissed: Tenants lose, landlords win in Baltimore rent court. %23rentcourt"; //Tweet text
 			var url = "http://data.baltimoresun.com/news/dismissed/"; //Interactive URL
 
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
@@ -20,8 +20,8 @@ var app = app || {
 		$(".icon-facebook").on("click", function(){
 
 			var picture = "http://data.baltimoresun.com/news/dismissed/dist/images/facebook-thumb-rent-court.png"; //Picture URL
-			var title = "Dismissed"; //Post title
-			var description = "Tenants lose, landlords win in Baltimore rent court"; //Post description
+			var title = "Dismissed: Tenants lose, landlords win in Baltimore rent court"; //Post title
+			var description = "Read the Baltimore Sun investigation"; //Post description
 			var url = "http://data.baltimoresun.com/news/dismissed/"; //Interactive URL
 
 	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
